9b915cf2882c7044f3a2bdcafdb944bc774c0e20
ShabazBadshah <badshah.shabaz@gmail.com>
2017-12-23 21:01:07 -0500

    fixed an index-out-of-bounds bug in the Stacks.py file, also finished the writeup and implementation of the Queue ADT

--- Queue/Queue.py
+++ Queue/Queue.py
@@ -1,52 +1,54 @@
-class Stack():
+class Queue():
 
     def __init__(self):
         self.__array = []
         self.__size = 0
 
-    def push(self, x):
+    def enqueue(self, x):
         self.__size += 1
         self.__array.append(x)
+
+    def front(self):
+        if (self.__size > 0):
+            return self.__array[0]
+        return None
         
-    def pop(self):
+    def dequeue(self):
         if (self.__size > 0):
             self.__size -= 1
-            return self.__array.pop()
+            return self.__array.pop(0)
         return None
 
     def search(self, x):
         for i in range(len(self.__array)):
-            if (x == self.__array[i]):
+            if (self.__array[i] == x):
                 return True
         return False
-
-    def peek(self):
-        if (self.__size > 0):
-            return self.__array[self.__size - 1]
-        return None
 
     def size(self):
         return len(self.__array)
 
     def __len__(self):
         return self.__size
-
+    
     def __str__(self):
         return str(self.__array)
 
 
-s = Stack()
-s.push(1)
-s.push(2)
-s.push(-3)
-s.push(42)
-print(s)
-print(s.pop())
-print(s)
-print(s.pop())
-print(s.peek())
-print(s)
-print(s.size())
-print(s.search(-99))
-print(s.search(-3))
-print(s.search(2))
+q = Queue()
+
+print(q)
+q.enqueue(1)
+q.enqueue(2)
+q.enqueue(3)
+print(q)
+print(q.front())
+print(q.dequeue())
+print(q)
+print(q.front())
+print(q.search(3))
+print(q)
+print(q.search(2))
+print(q.dequeue())
+print(q.dequeue())
+print(q)
