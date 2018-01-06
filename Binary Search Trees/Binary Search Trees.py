6955b516a2dc02edaa0a905e956213fa5e5b76ed
ShabazBadshah <badshah.shabaz@gmail.com>
2018-01-06 14:47:49 -0500

    implemented bst_insert

--- Binary Search Trees/Binary Search Trees.py
+++ Binary Search Trees/Binary Search Trees.py
@@ -1,4 +1,4 @@
-class Node:
+class node:
     def __init__(self, data, left_child=None, right_child=None):
         self.__data = data
         self.__left_child = left_child
@@ -10,10 +10,10 @@ class Node:
     def right_child(self):
         return self.__right_child
 
-    def set_left_node(self, new_left_child):
+    def set_left_child(self, new_left_child):
         self.__left_child = new_left_child
 
-    def set_right_node(self, new_right_child):
+    def set_right_child(self, new_right_child):
         self.__right_child = new_right_child
 
     def get_data(self):
@@ -32,11 +32,81 @@ class Node:
         return "[{0}:{1}:{2}]".format(self.__data, left_str, right_str)
 
 
-n0 = Node(3)
-print n0
-n1 = Node(2)
-n2 = Node(4, n0, n1)
-print n1
-print n2
-n0.set_left_node(n1)
-print n2
+'''
+Binary Search Tree Property:
+
+For any given node, all nodes in the left subtree will have values that are
+strictly less than the root. All nodes in the right subtree will have values
+that are greater than or equal to the root's value
+
+Space Complexity: O(n)
+'''
+class binary_search_tree:
+    def __init__(self, root):
+        self.__root = root
+
+    '''
+    Runtime: O(n) worst case, O(log n) average
+    '''
+    def insert(self, root, node):
+
+        if root == None:
+            root = node
+            return True
+        else:
+            if node.get_data() >= root.get_data():
+                if root.left_child() == None:
+                    root.set_left_child(node)
+                    return True
+                else:
+                    return self.insert(root.left_child(), node)
+            else:
+                if root.right_child() == None:
+                    root.set_right_child(node)
+                    return True
+                else:
+                    return self.insert(root.right_child(), node)
+        return False
+
+
+    def delete(self, root, node):
+        pass
+
+    '''
+    Runtime: O(n) worst case, O(log n) average
+    '''
+    def search(self, node, element_to_find):
+
+        if node.get_data() == element_to_find:
+            return node
+
+        if element_to_find >= node.get_data():
+            return search(node.right_child(), element_to_find)
+        else:
+            return search(node.left_child(), element_to_find)
+
+        return None
+
+
+    '''
+    Runtime: O(1)
+    '''
+    def get_root(self):
+        return self.__root
+
+    def __str__(self):
+        return str(self.__root)
+
+root = node(0)
+n1 = node(2)
+n2 = node(1)
+n3 = node(-1)
+n4 = node(-2)
+
+bst = binary_search_tree(root)
+print bst
+print bst.insert(root, n1)
+print bst.insert(root, n2)
+print bst.insert(root, n3)
+print bst.insert(root, n4)
+print bst
