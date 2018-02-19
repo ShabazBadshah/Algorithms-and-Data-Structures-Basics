1aa2a9cfebd21539bbaaca8b359bf9403f097a8b
Shabaz Badshah <badshah.shabaz@gmail.com>
2018-02-19 00:24:17 -0500

    implemented selection sort

---
+++ sorting/selection_sort.py
@@ -0,0 +1,24 @@
+'''
+Runtime Worst Case: O(n^2)
+Runtime Avg Case: O(n^2)
+Runtime Best Case: O(n^2)
+Space: O(1)
+'''
+
+def selection_sort(arr):
+
+    for i in range(len(arr)):
+        
+        max_ind = i
+
+        for j in range (i, len(arr)):
+            if (arr[j] > arr[max_ind]): 
+                max_ind = j
+
+        arr[i], arr[max_ind] = arr[max_ind], arr[i]     
+
+    return arr
+
+
+array = [9, 3, 4, 1, 7, 2, 10, 6, 8, 5]
+print(selection_sort(array))
