155a21ea4c59ad13cf9e72666b7c712c780920d4
Shabaz Badshah <badshah.shabaz@gmail.com>
2018-02-21 23:48:12 -0500

    added an explanation to insertion_sort.py file about how the algorithm works at a high level

--- sorting/insertion_sort.py
+++ sorting/insertion_sort.py
@@ -4,6 +4,14 @@ Runtime Avg Case: O(n^2)
 Runtime Best Case: O(n)
 Space: O(1)
 '''
+
+'''
+The crux of insertion sort is comparable to sorting a deck of cards. The algorithm keeps a 
+sorted and unsorted list. Similarly to how you may sort a deck of cards, the algorithm will 
+pick the next element from the unsorted part of the list and 'insert' it into the correct spot
+in the sorted part of the list. This repeats until the list is sorted 
+'''
+
 def insertion_sort(arr):
 
     for i in range(0, len(arr)):
@@ -12,7 +20,7 @@ def insertion_sort(arr):
         pos = i
 
         while (pos > 0 and arr[pos - 1] > curr_el):
-            arr[pos] = arr[pos - 1]
+            arr[pos], arr[pos - 1] = arr[pos - 1], arr[pos]
             pos -= 1
         arr[pos] = curr_el
 
