b4d5cf30e70d7e7707929de40fa1bfb85893969c
Shabaz Badshah <badshah.shabaz@gmail.com>
2018-02-22 00:02:05 -0500

    added an explanation to selection_sort.py file about how the algorithm works at a high level

--- sorting/selection_sort.py
+++ sorting/selection_sort.py
@@ -5,6 +5,20 @@ Runtime Best Case: O(n^2)
 Space: O(1)
 '''
 
+'''
+Selection sort is very similar to insertion sort in terms of keeping track of a sorted and
+unsorted array, however selection sort differs in the way it adds elements to the sorted portion
+of the array. More specifically, selection sort will 'select' the minimum element from the 
+unsorted array and appends it to the sorted portion of the array. 
+
+Ex.
+
+sorted              sorted                  sorted
+ v                    v                       v
+[3, | 9, 7, 5] -> [3, 5, | 9, 7] -> [3, 5, 7, | 9] -> [3, 5, 9, 7]
+
+'''
+
 def selection_sort(arr):
 
     for i in range(len(arr)):
@@ -13,7 +27,8 @@ def selection_sort(arr):
         j = i
 
         while (j < len(arr)): 
-            if (arr[j] > arr[max_ind]): max_ind = j
+            if (arr[j] > arr[max_ind]): 
+                max_ind = j
             j += 1
 
         arr[i], arr[max_ind] = arr[max_ind], arr[i]     
