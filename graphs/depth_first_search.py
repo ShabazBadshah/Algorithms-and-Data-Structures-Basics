6476f6edea4f0aa46202ef9ebd229446f8bb5f20
Shabaz Badshah <badshah.shabaz@gmail.com>
2018-03-25 00:04:53 -0400

    added a very brief overview about dfs in depth_first_search.py

--- graphs/depth_first_search.py
+++ graphs/depth_first_search.py
@@ -2,13 +2,18 @@
 Runtime (Adjacency List): O(|V|+|E|)
 Space (Adjacency List): O(|V|+|E|)
 
-Returns a traversal of the graph where all child nodes are visited first before neighbour nodes
+Returns a traversal of the graph where all child nodes are visited first before neighbours nodes
+
+Other Notes:
+- Can be used to find if a path exists between two vertices (path finding)
+- Is used to determine if a cycle exists in a graph
+- Used to solve puzzles that only have one solution (i.e. mazes)
 '''
 
 def dfs(g, node_to_start_from):
 
     if not g[node_to_start_from]:
-        return
+        return []
 
     visited = []
     stack = [node_to_start_from]
@@ -23,7 +28,6 @@ def dfs(g, node_to_start_from):
             for child in children:
                 stack.append(child)
 
-
     return visited
 
 
@@ -34,4 +38,4 @@ graph = {'A': ['B', 'C', 'E'],
          'E': ['C']
          }
     
-print dfs(graph, 'A')
+print (dfs(graph, 'A'))
