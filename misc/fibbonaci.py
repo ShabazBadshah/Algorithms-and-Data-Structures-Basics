b5a3c02499a12445404526615e4c6f783921b748
Shabaz Badshah <badshah.shabaz@gmail.com>
2018-04-12 20:49:14 -0400

    implemented fibonnaci sequence with O(1) space

--- misc/fibbonaci.py
+++ misc/fibbonaci.py
@@ -1,5 +1,4 @@
 def print_fibbonaci(num_of_elements):
-    
     fib = [1] 
 
     if (num_of_elements <= 2):
@@ -10,7 +9,19 @@ def print_fibbonaci(num_of_elements):
         
     return fib[num_of_elements - 1]
 
+
+def print_fibbonaci_space(num_of_elements):
+    a, b = 0, 1
+    for i in range(num_of_elements + 1):
+        a, b = b, a + b
+    return a
+
 print(print_fibbonaci(5))
 print(print_fibbonaci(10))
 print(print_fibbonaci(1000))
 print(print_fibbonaci(10000))
+
+print(print_fibbonaci_space(5))
+print(print_fibbonaci_space(10))
+print(print_fibbonaci_space(1000))
+print(print_fibbonaci_space(10000))
