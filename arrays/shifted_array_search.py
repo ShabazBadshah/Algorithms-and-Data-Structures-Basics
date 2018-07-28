5fe234eb3ae6c237e670cb2ec3845ccace7d544b
ShabazBadshah <badshah.shabaz@gmail.com>
2018-07-28 17:59:42 -0400

    optimized shifted_array_search.py

--- arrays/shifted_array_search.py
+++ arrays/shifted_array_search.py
@@ -1,52 +1,56 @@
 '''
-Given an array that has been shifted an unspecified amount
-of times, find the amount of times the array has been shifted
+Given an shiftArray that has been shifted an unspecified amount
+of times, find the amount of times the shiftArray has been shifted
 for some given number.
 
 If the number is not in the list, return -1, otherwise return the
 shifted amount
+
+Runs in O(log n)
 '''
 
-'''
-Note: 
-This solution is suboptimal as it runs in O(n) time, but it can
-be run in O(log n) time if the findPivot function is changed from 
-a linear search to a binary search (since our array is sorted)
-'''
 
 def shiftedArrSearch(shiftArr, num):
-    pivot = findPivot(shiftArr)
+  pivot = findPivot(shiftArr)
 
-    if(pivot == 0 or num < shiftArr[0]):
-        return binarySearch(shiftArr, pivot, len(shiftArr) - 1, num)
-    
-    return binarySearch(shiftArr, 0, pivot - 1, num)
+  if(pivot == 0 or num < shiftArr[0]):
+    return binarySearch(shiftArr, pivot, len(shiftArr) - 1, num)
+
+  return binarySearch(shiftArr, 0, pivot - 1, num)
+
 
 def findPivot(shiftArr):
-  shiftAmt = 1
-  for i in range(len(shiftArr) - 1):
-    if shiftArr[i + 1] <= shiftArr[i]:
-      break
+  begin = 0
+  end = len(shiftArr) - 1
+
+  while begin <= end:
+    mid = begin + (end - begin) // 2
+    if mid == 0 or shiftArr[mid] < shiftArr[mid - 1]:
+      return mid
+    if shiftArr[mid] > shiftArr[0]:
+      begin = mid + 1
     else:
-      shiftAmt += 1
-  return shiftAmt
+      end = mid - 1
+
+  return 0
 
 
-def binarySearch(arr, begin, end, num):
-    while (begin <= end):
-        mid = begin + (end - begin)//2
-        if (arr[mid] < num):
-            begin = mid + 1
-        elif (arr[mid] == num):
-            return mid
-        else:
-            end = mid - 1
+def binarySearch(shiftArr, begin, end, num):
+  while (begin <= end):
+    mid = begin + (end - begin)//2
+    if (shiftArr[mid] < num):
+      begin = mid + 1
+    elif (shiftArr[mid] == num):
+      return mid
+    else:
+      end = mid - 1
 
-    return -1
+  return -1
 
-arr = [9, 12, 17, 2, 4, 5]
-print (shiftedArrSearch(arr, 2)) # 3
-print (shiftedArrSearch(arr, 12)) # 1
-print (shiftedArrSearch(arr, 17)) # 2
-print (shiftedArrSearch(arr, 4)) # 4
-print (shiftedArrSearch(arr, -1)) # -1
+
+shiftArr = [9, 12, 17, 2, 4, 5]
+print (shiftedArrSearch(shiftArr, 2))  # 3
+print (shiftedArrSearch(shiftArr, 12))  # 1
+print (shiftedArrSearch(shiftArr, 17))  # 2
+print (shiftedArrSearch(shiftArr, 4))  # 4
+print (shiftedArrSearch(shiftArr, -1))  # -1
