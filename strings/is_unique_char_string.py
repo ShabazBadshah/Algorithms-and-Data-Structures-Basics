7bcf28fa218cad066da77a2ca78c230d152d99aa
ShabazBadshah <badshah.shabaz@gmail.com>
2018-08-18 20:01:19 -0400

    added new implementation of is_unique_string_char

--- strings/is_unique_char_string.py
+++ strings/is_unique_char_string.py
@@ -7,9 +7,19 @@ def is_unique_char_string(string):
             str_hash[letter] = ''
     return True
 
+def is_unique_char_string_set(string):
+    return len(string) == len(set(string))
+
 # Runs in O(n)
 print (is_unique_char_string("abc"))
 print (is_unique_char_string(""))
 print (is_unique_char_string("racecar"))
 print (is_unique_char_string("aA"))
 print (is_unique_char_string("potato"))
+
+print ("-----Set Implentation-----")
+print (is_unique_char_string_set("abc"))
+print (is_unique_char_string_set(""))
+print (is_unique_char_string_set("racecar"))
+print (is_unique_char_string_set("aA"))
+print (is_unique_char_string_set("potato"))
