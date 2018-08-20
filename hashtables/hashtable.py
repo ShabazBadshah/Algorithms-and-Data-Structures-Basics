b5f82be1357b36b59b8869b5e40735fea4adcb76
ShabazBadshah <badshah.shabaz@gmail.com>
2018-08-19 21:04:12 -0400

    simplified hashtable implementation

--- hashtables/hashtable.py
+++ hashtables/hashtable.py
@@ -1,53 +1,36 @@
 import math
 
 class Hashtable:
-
   def __init__(self, size = 32):
     # Gets the next highest number that is a power of 2
     self.__size = 2 ** (int(math.ceil(math.log(size)/math.log(2))))
-    self.__table = self.__size * [None]
+    self.__table = [[] for _ in range(self.__size)]
 
   def __get_index(self, element):
     return hash(element) % self.__size
 
   def get(self, element):
     idx = self.__get_index(element)
-
-    if type(self.__table[idx]) == list:
-      return element in self.__table[idx]
-    else:
-      return self.__table[idx] == element
-
-    return None
+    return element in self.__table[idx]
 
   def put(self, element):
     idx = self.__get_index(element)
-
-    if not self.__table[idx]:
-      self.__table[idx] = element
-    else:
-      if type(self.__table[idx]) == list:
-        self.__table[idx] = [self.__table[idx], element]
-      else:
-        self.__table.append(element)
+    self.__table[idx].append(element)
 
   def delete(self, element):
     idx = self.__get_index(element)
-
-    if self.__table[idx] != None:
-      if type(self.__table[idx]) == list:
-        element_idx = self.__table[idx].index(element)
-        self.__table[idx][element_idx] = None
-      else:
-        self.__table[idx] = None
-    else:
-      raise KeyError("element" + str(element) + "can't be found")
+    try:
+      element_idx = self.__table[idx].index(element)
+      self.__table[idx][element_idx] = None
+    except ValueError:
+      raise KeyError("KeyError: " + str(element) + " was not found")
 
   def __str__(self):
     return str(self.__table)
 
-h = Hashtable(20)
+h = Hashtable(30)
 h.put('abc')
-print(h.get('abc'))
-h.delete('abc')
-print(h.get('abc'))
+print(h.get('abc')) # True
+h.delete('abc') 
+print(h.get('abc')) # False
+print(h.delete('abc')) # Raises KeyError, program terminates
